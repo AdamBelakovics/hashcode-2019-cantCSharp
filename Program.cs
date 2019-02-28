2e3eb34f81361d8d4e3ab89d2bf1b18e9e0647e5
AdamBelakovics <beladi96@gmail.com>
2019-02-28 18:18:38 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hascode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ádám vok cső");
+        }
+    }
+}
