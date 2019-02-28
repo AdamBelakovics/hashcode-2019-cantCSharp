8c4500f05bc51e8e97e9ad518e3302067278b4e7
matesebi <matesebi96@gmail.com>
2019-02-28 19:36:30 +0100

    added grading and outputing to file

--- Slideshow.cs
+++ Slideshow.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace hascode
 {
 	class Slideshow
 	{
-		public List<Picture> Pictures { get; set; }
-		public int Score { get; set; }
+		public List<ISlide> Slides { get; set; }
+
+		public int Score
+		{
+			get
+			{
+				var sum = 0;
+				for (var i = 0; i < Slides.Count - 1; i++)
+				{
+					var first = Slides[i].GetTags();
+					var second = Slides[i + 1].GetTags();
+					var union = first.Intersect(second).Count();
+					var except1 = first.Except(second).Count();
+					var except2 = second.Except(first).Count();
+					sum += Math.Min(union, Math.Min(except1, except2));
+				}
+				return sum;
+			}
+		}
+
+		public void prnintOutputTo(string outputPath)
+		{
+			using (var sw = new StreamWriter(outputPath))
+			{
+				sw.WriteLine(Slides.Count);
+				foreach (var slide in Slides)
+				{
+					sw.WriteLine(slide.GetIds());
+				}
+			}
+		}
+
 	}
 }
