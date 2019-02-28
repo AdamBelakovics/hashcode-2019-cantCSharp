acadb2b8f4b07b7666de57fa59ea85cf9f38a245
AdamBelakovics <beladi96@gmail.com>
2019-02-28 20:30:09 +0100

    Greedy 0.1

--- Greedy.cs
+++ Greedy.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using hascode;
 
@@ -9,7 +10,24 @@ namespace hascode
     {
         public Slideshow execute(PictureStore pictureStore)
         {
+            var slides = createSlides(pictureStore);
+            var orderedSlides = slides.OrderByDescending(s1 => s1.GetTags().Count);
+            var result = new Slideshow();
+            while(orderedSlides.Count() != 0){
+
+            }
             return new Slideshow();
+        }
+
+        public List<ISlide> createSlides(PictureStore store){
+            var ss = new List<ISlide>();
+            foreach(var pic in store.HorizontalPictures){
+                var s = new HorizontalSlide();
+                s.Picture = pic;
+                ss.Add(s);
+            }
+            //TODO Horizontal
+            return ss;
         }
     }
 }
